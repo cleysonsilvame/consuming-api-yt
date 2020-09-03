cb35281f547a5e7d378a38c5e4c72dc39e7bafcc
lukesodre <lucasasodre@gmail.com>
2020-09-02 23:48:16 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,22 @@ const routes = express.Router()
 const AuthController = require('./controllers/authController')
 const ApiYouTubeController = require('./controllers/apiYouTubeController')
 
+let comentario = {};
 routes.get('/', (req, res) => {
   return res.json(
     {
       message: 'For more information access the documentation!',
       link: 'https://github.com/cleysonsilvame/consuming-api-yt'
     })
+})
+
+routes.post('/comentario',(req,res)=>{
+   comentario = req.body.comentario;
+  res.json(comentario);
+})
+
+routes.get('/comentario',(req,res)=>{
+  res.json(comentario);
 })
 
 routes.get('/auth', AuthController.getAuthentication)
