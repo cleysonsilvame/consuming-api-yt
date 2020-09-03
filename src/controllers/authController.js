8c6f01eb4b03ee667095ca054f315dac33673fb6
Cleyson Silva <62582404+cleysonsilvame@users.noreply.github.com>
2020-09-03 16:45:31 -0300

    incomplete implementation views with auth api

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,37 +2,30 @@ const { google } = require('googleapis')
 const OAuth2 = google.auth.OAuth2
 // require('dotenv').config()
 
-const credentials = require('../credentials/client_secret_164362257431-2ujlfqu0fvp9m79mnbkahqd52b869u6h.apps.googleusercontent.com.json')
-const OAuthClient = new OAuth2(
-  credentials.web.client_id,
-  credentials.web.client_secret,
-  credentials.web.redirect_uris[0]
-)
 
 module.exports = {
-  async getAuthentication(req, res){
-    const consentUrl = OAuthClient.generateAuthUrl({
-      access_type: 'offline',
-      scope: ['https://www.googleapis.com/auth/youtube', 'https://www.googleapis.com/auth/youtube.readonly', 'https://www.googleapis.com/auth/youtube.force-ssl']
-    })
-    res.json(
-    {
-      message: 'Click on link to auth',
-      consentUrl: consentUrl,
-    })
-  },
+  async getAuthentication(req, res) {
+    async createOAuthClient(){
+      const credentials = require('../credentials/client_secret_164362257431-2ujlfqu0fvp9m79mnbkahqd52b869u6h.apps.googleusercontent.com.json')
 
-  async setSessionAuth(req, res){
-    const OAuthClient = new OAuth2(
-      credentials.web.client_id,
-      credentials.web.client_secret,
-      credentials.web.redirect_uris[0]
-    )
-
+      
+      const OAuthClient = new OAuth2(
+        credentials.web.client_id,
+        credentials.web.client_secret,
+        credentials.web.redirect_uris[0]
+        )
+      }
+      
+      
+      const consentUrl = OAuthClient.generateAuthUrl({
+        access_type: 'offline',
+        scope: ['https://www.googleapis.com/auth/youtube', 'https://www.googleapis.com/auth/youtube.readonly', 'https://www.googleapis.com/auth/youtube.force-ssl']
+      })
+    
     const authCode = req.query.code
     res.send('Authentication success. Thank you!')
     OAuthClient.setCredentials((await OAuthClient.getToken(authCode)).tokens)
-  
+
     google.options({
       auth: OAuthClient
     })
--- src/server.js
+++ src/server.js
@@ -9,7 +9,6 @@ app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'handlebars')
 
-
 app.use(express.json())
 app.use(routes)
 
